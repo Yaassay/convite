a53bbeade72cc64b76e28972bdef946a212c2ab9
Yaassay <160131110+Yaassay@users.noreply.github.com>
2024-12-18 20:46:10 -0300

    Update convite.js

--- convite.js
+++ convite.js
@@ -1,13 +1,13 @@
 document.getElementById('yesBtn').addEventListener('click', function() {
     // Esconde o conteúdo inicial
     document.querySelector('.content').classList.add('hidden');
-    // Exibe a imagem em tela cheia
+    // Exibe a primeira imagem surpresa
     document.getElementById('image-container').classList.remove('hidden');
 });
 
 document.getElementById('noBtn').addEventListener('click', function() {
     // Esconde o conteúdo inicial
     document.querySelector('.content').classList.add('hidden');
-    // Exibe a mensagem de spam
-    document.getElementById('spam-message').classList.remove('hidden');
+    // Exibe a segunda imagem surpresa
+    document.getElementById('image-container-no').classList.remove('hidden');
 });
